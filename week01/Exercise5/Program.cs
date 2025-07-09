eb20e8033a80019c3bb1f0a488fa8cb757d8061f
Adesax27 <ademolaokunjoyo@gmail.com>
2025-07-09 19:09:35 +0100

    Made new changes

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,49 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void DisplayWelcome()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a whole number.");
+            return PromptUserNumber(); // Recursive call to ask again
+        }
+    }
+
+    public static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    public static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+
+    public static void Main(string[] args)
+    {
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
     }
 }
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,49 +1,9 @@
 using System;
 
-public class Program
+class Program
 {
-    public static void DisplayWelcome()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the program!");
-    }
-
-    public static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        return Console.ReadLine();
-    }
-
-    public static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        string input = Console.ReadLine();
-        if (int.TryParse(input, out int number))
-        {
-            return number;
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a whole number.");
-            return PromptUserNumber(); // Recursive call to ask again
-        }
-    }
-
-    public static int SquareNumber(int number)
-    {
-        return number * number;
-    }
-
-    public static void DisplayResult(string name, int squaredNumber)
-    {
-        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
-    }
-
-    public static void Main(string[] args)
-    {
-        DisplayWelcome();
-        string userName = PromptUserName();
-        int favoriteNumber = PromptUserNumber();
-        int squaredNumber = SquareNumber(favoriteNumber);
-        DisplayResult(userName, squaredNumber);
+        Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
