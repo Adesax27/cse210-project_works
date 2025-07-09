6581602ed3b2755efc107db3f75ee3183d2d7928
Adesax27 <ademolaokunjoyo@gmail.com>
2025-07-09 19:29:07 +0100

    Expanded on week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Data Analyst";
+        job1._company = "Amazon";
+        job1._startYear = 2016;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Chief Tech Officer";
+        job2._company = "Tesla";
+        job2._startYear = 2019;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ademola Okunjoyo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
